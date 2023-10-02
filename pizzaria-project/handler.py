1cd0343c148f5c4b32ae666ef54e3f88b0f2509e
andrewsfs <andrewsfs@gmail.com>
2023-10-02 01:34:31 +0000

    Atualiza Serverless

--- pizzaria-project/handler.py
+++ pizzaria-project/handler.py
@@ -5,27 +5,33 @@ dynamodb = boto3.client('dynamodb')
 sqs = boto3.client('sqs')
 
 def processPedido(event, context):
-    for record in event['Records']:
-        evento = json.loads(record['body'])
+    if 'Records' in event:
+        for record in event['Records']:
+            evento = json.loads(record['body'])
 
-        response = dynamodb.put_item(
-            TableName='eventos-pizzaria',
-            Item={
-                'pedido': {'S': evento['pedido']},
-                'status': {'S': evento['status']}
-            }
-        )
-
-        print(f"Evento gravado no DynamoDB: {response}")
-
-def enviarParaFilaSQS(event, context):
-    for record in event['Records']:
-        evento = json.loads(record['body'])
-
-        if evento['status'] == 'pronto':
-            response = sqs.send_message(
-                QueueUrl='https://sqs.us-east-1.amazonaws.com/051246040808/espera-entrega',
-                MessageBody=json.dumps(evento)
+            response = dynamodb.put_item(
+                TableName='eventos-pizzaria',
+                Item={
+                    'pedido': {'S': evento['pedido']},
+                    'status': {'S': evento['status']}
+                }
             )
 
-            print(f"Mensagem enviada para a fila de entrega: {response}")
+            print(f"Evento gravado no DynamoDB: {response}")
+    else:
+        print("Evento não possui a chave 'Records'")
+
+def enviarParaFilaSQS(event, context):
+    if 'Records' in event:
+        for record in event['Records']:
+            evento = json.loads(record['body'])
+
+            if evento['status'] == 'pronto':
+                response = sqs.send_message(
+                    QueueUrl='https://sqs.us-east-1.amazonaws.com/051246040808/espera-entrega',
+                    MessageBody=json.dumps(evento)
+                )
+
+                print(f"Mensagem enviada para a fila de entrega: {response}")
+    else:
+        print("Evento não possui a chave 'Records'")
